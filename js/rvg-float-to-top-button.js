6b80802e608799e3d537e2b13f3fb7119994302d
cageehv <cageehv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-08 21:00:15 +0000

    Initial release
    git-svn-id: https://plugins.svn.wordpress.org/float-to-top-button/tags/1.0@1022201 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/rvg-float-to-top-button.js
@@ -0,0 +1,42 @@
+/* ---------------------------------------------------
+ *
+ *	Initialize the scrollUp object
+ *
+ *	http://markgoodyear.com/2013/01/scrollup-jquery-plugin/
+ *
+ *	Options
+ *	scrollName: 'scrollUp',
+ *	topDistance: '300', // Distance from top before showing element (px)
+ *	topSpeed: 300, // Speed back to top (ms)
+ *	animation: 'fade', // Fade, slide, none
+ *	animationInSpeed: 200, // Animation in speed (ms)
+ *	animationOutSpeed: 200, // Animation out speed (ms)
+ *	scrollText: 'Scroll to top', // Text for element
+ *	scrollImg: false,
+ *	activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF' 
+ *
+ * -------------------------------------------------*/	
+jQuery(document).ready(function(){
+	/* CREATE THE SCROLLUP INSTANCE */
+	jQuery.scrollUp({
+		topDistance: fttb_topdistance,             // DISTANCE FROM TOP BEFORE SHOWING ELEMENT (PX)
+		topSpeed: fttb_topspeed,                   // SPEED BACK TO TOP (MS)
+		animation: fttb_animation,                 // FADE, SLIDE, NONE
+		animationInSpeed: fttb_animationinspeed,   // ANIMATION IN SPEED (MS)
+		animationOutSpeed: fttb_animationoutspeed, // ANIMATION OUT SPEED (MS)
+		scrollText: fttb_scrolltext                // TTTLE FOR THE IMAGE
+	});
+	
+	/* SET THE 'TO TOP' IMAGE TO THE SELECTED IMAGE */
+	jQuery("#scrollUp").css({"background-image":"url("+fttb_imgurl+fttb_arrow_img+")"});
+	
+	/* SET THE OPACITY OF THE 'TO TOP' IMAGE (FROM THE OPTIONS) */
+	jQuery("#scrollUp").css({"-khtml-opacity":"."+fttb_opacity});
+	jQuery("#scrollUp").css({"-moz-opacity":"."+fttb_opacity});
+	jQuery("#scrollUp").css({"-ms-filter":'"alpha(opacity='+fttb_opacity+')"'});
+	jQuery("#scrollUp").css({"filter":"alpha(opacity="+fttb_opacity+")"});
+	jQuery("#scrollUp").css({"filter":"progid:DXImageTransform.Microsoft.Alpha(opacity=0."+fttb_opacity+")"});
+	jQuery("#scrollUp").css({"opacity":"."+fttb_opacity});	
+});		
+
+
